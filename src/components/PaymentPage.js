d92f716a7ea529b3f04e1fa19bfb86ee40663a65
[moechamas] <[moechamas@outlook.com]>
2024-02-17 01:07:48 -0500

    save

--- src/components/PaymentPage.js
+++ src/components/PaymentPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import { CartContext } from './CartContext';
 import { OrdersContext } from './OrdersContext';
